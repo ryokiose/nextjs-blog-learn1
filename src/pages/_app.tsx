e70a7cd2f8f2481c05cfe6e6bf2e58febec34888
ryoki <mfjzc4xh2@gmail.com>
2023-11-07 10:55:59 +0900

    hotfix: Fix_app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,7 @@
-import { useEffect } from "react";
-import "@/styles/global.css";
-import { AppProps } from "next/app";
-import nprogress from "nprogress";
-import "nprogress/nprogress.css";
-import Header from "@/components/Header";
+import type { AppProps } from "next/app";
 
-nprogress.configure({ showSpinner: false, speed: 400, minimum: 0.25 });
-
-const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
-	if (typeof window !== "undefined") {
-		nprogress.start();
-	}
-
-	useEffect(() => {
-		nprogress.done();
-	});
-	return (
-		<>
-			<Header />
-			<Component {...pageProps} />
-		</>
-	);
+const App = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
 };
 
 export default App;

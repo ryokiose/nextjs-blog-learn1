39bcbbc0826aa3b45992cba59cf9fd66b6b486b0
ryoki <mfjzc4xh2@gmail.com>
2023-11-07 11:29:02 +0900

    feat: posts/first-posts.tsx

--- src/pages/posts/first-posts.tsx
+++ src/pages/posts/first-posts.tsx
@@ -1,5 +1,9 @@
+import Link from 'next/link';
+
 const FirstPost = () => {
-	return <h1>First Post</h1>;
+	return (
+    <><h1>First Post</h1><Link href={"/"}>Back to Home</Link></>
+	);
 };
 
 export default FirstPost;
